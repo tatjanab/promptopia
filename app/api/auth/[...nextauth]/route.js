4b848bed09f22fd9206bccd48f5079a218dd3660
Tatjana Boskovic <tb@linkmedia.com>
2024-07-10 11:07:23 +0400

    Oauth login successfully added

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,17 +1,56 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: "",
-      clientSecret: "",
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 
-  async session({ session }) {},
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
 
-  async signIn({ profile }) {},
+      session.user.id = sessionUser._id.toString();
+
+      return session;
+    },
+
+    async signIn({ profile }) {
+      try {
+        // serverless -> Lambda -> dynamodb
+        await connectToDB();
+        console.log("Profile:", profile);
+        // check if a user already exists
+        const userExists = await User.findOne({
+          email: profile.email,
+        });
+        // if not, create a new user
+        console.log("userExists: ", userExists);
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(/ /g, "").toLowerCase(),
+            image: profile.picture,
+          });
+
+          return true;
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
@@ -27,12 +27,13 @@ const Nav = () => {
           width={30}
           height={30}
           className="object-contain"
+          alt="logo"
         />
         <p className="logo_text">Promptopia</p>
       </Link>
 
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -42,7 +43,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -67,10 +68,10 @@ const Nav = () => {
         )}
       </div>
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
